d028c7d1a1e3cf1a1c414d512d99590500515112
Eric0329 <ericyang0329@gmail.com>
2020-02-05 16:25:35 +0800

    Update index.js

--- csv_custs_mangento2ecount/index.js
+++ csv_custs_mangento2ecount/index.js
@@ -19,10 +19,10 @@ const moment = require('moment');
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
  */
-exports.orderFilter = (data, context, callback) => {
+exports.orderFilter = (event, context, callback) => {
     console.log(`Event Type: ${context.eventType}`);
 
-    const file = data;
+    const file = event;
     var bucketName   = file.bucket;
     var pathFileName = file.name;
 
